7fb3fad87dbd6413acda3e0899d893e44a7ea56f
floetenheini <fischerjonas@mediamarktsaturn.de>
2023-11-03 15:20:22 +0100

    Hello World

--- src/app/second/page.tsx
+++ src/app/second/page.tsx
@@ -11,7 +11,7 @@ async function getPosts(): Promise<Post[]> {
   });
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
-
+  console.log("Hallo Welt");
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
